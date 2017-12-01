0e55fd61b830c4feb136eaccd510ad849f1ce168
shark <shark526.gm@gmail.com>
2017-12-01 10:55:51 +0800

    removed the unformated comments

--- client/plugins/Hass.py
+++ client/plugins/Hass.py
@@ -45,11 +45,6 @@ def hass(text, mic, profile):
             entity = requests.get(url_entity, headers=headers).json()
             devices.append(entity)
     for device in devices:
-        #name = device["attributes"]["friendly_name"]
-        state = device["state"]
-        attributes = device["attributes"]
-        domain = device["entity_id"].split(".")[0]
-        if 'dingdang' in attributes.keys():
             dingdang = attributes["dingdang"]
             if isinstance(dingdang, list):
                 if text in dingdang:
